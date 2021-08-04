4c206a532a5235cbea040450618da30ae8b23742
Shubham Jadhav <71382991+Shubham-web2144@users.noreply.github.com>
2021-08-04 09:55:29 +0530

    Add files via upload

---
+++ btn.js
@@ -0,0 +1,14 @@
+function btnFun() {
+    
+    const btn = document.querySelector('.btnContent');
+    
+    const btnAlter = document.querySelector('.btnAlter');
+    
+    const btnBox = document.querySelector('.btn');
+    
+    btn.classList.toggle('active');
+    
+
+    //btnAlter.classList.toggle('btnUp');
+
+}
